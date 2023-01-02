ab3b5a0f613a194f98b5b1ca858f6334dbcc9a0e
Maciej Lyskawinski <lyskawinski.maciej@gmail.com>
2023-01-02 13:14:34 +0100

    fix: user role change validation

--- apps/auth/src/admin/application/request/user-role.request.dto.ts
+++ apps/auth/src/admin/application/request/user-role.request.dto.ts
@@ -1,9 +1,11 @@
-import { IsString, IsUUID } from 'class-validator';
+import { IsDefined, IsString, IsUUID } from 'class-validator';
 
 export class UserRoleRequestDto {
+  @IsDefined()
   @IsUUID(4)
   userId: string;
 
+  @IsDefined()
   @IsString()
   role: string;
 }
